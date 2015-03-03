272a4c033cb5de5053f1e838f6449ca04dc448f7
Elvis Nuñez <elvisnunez@me.com>
2015-03-03 01:13:07 +0100

    Clean up project

--- Dream/AppDelegate.h
+++ Dream/AppDelegate.h
@@ -1,25 +1,8 @@
-//
-//  AppDelegate.h
-//  Dream
-//
-//  Created by Elvis Nuñez on 2/25/15.
-//  Copyright (c) 2015 Elvis Nuñez. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-#import <CoreData/CoreData.h>
+@import UIKit;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
-
 
 @end
 
