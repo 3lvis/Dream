f3eed2e695e424e74ff5c689c22d793be0e37ab9
Elvis Nuñez <elvisnunez@me.com>
2015-03-03 22:03:03 +0100

    Make main controller a UITableViewController

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -1,12 +1,26 @@
 import UIKit
 
-class ViewController: UIViewController
+class ViewController: UITableViewController
 {
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.view.backgroundColor = UIColor.redColor()
+        self.tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: "Cell")
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1;
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3;
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+        cell.textLabel!.text = "Hello"
+
+        return cell
     }
 }
 
